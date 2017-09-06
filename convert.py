dd93d952bb35a4da2364faf8bc89b4db8f7ba53f
khanrc <khanrc@naver.com>
2017-09-06 17:32:25 +0900

    refactoring convert.py

--- convert.py
+++ convert.py
@@ -28,6 +28,7 @@ def center_crop(im, output_height, output_width):
 
 def convert(source_dir, target_dir, exts=['jpg'], num_shards=128, tfrecords_prefix=''):
 	if not tf.gfile.Exists(source_dir):
+		print('source_dir does not exists')
 		return
 	
 	if tfrecords_prefix and not tfrecords_prefix.endswith('-'):
@@ -43,57 +44,51 @@ def convert(source_dir, target_dir, exts=['jpg'], num_shards=128, tfrecords_pref
 	# get meta-data
 	path_list = []
 	for ext in exts:
-		path = os.path.join(source_dir, '*.' + ext)
+		pattern = '*.' + ext if ext != '' else '*'
+		path = os.path.join(source_dir, pattern)
 		print path
 		path_list.extend(glob.glob(path))
 
-	# shuffle path_list ? 
+	# shuffle path_list
 	np.random.shuffle(path_list)
 	num_files = len(path_list)
 	num_per_shard = num_files // num_shards # 마지막 샤드는 더 많음
 
-	print '# of files: {}'.format(num_files)
-	print '# of shards: {}'.format(num_shards)
-	print '# files per shards: {}'.format(num_per_shard)
+	print('# of files: {}'.format(num_files))
+	print('# of shards: {}'.format(num_shards))
+	print('# files per shards: {}'.format(num_per_shard))
 
 	# convert to tfrecords
-	i = 0
-	for shard_idx in range(num_shards):
-		tfrecord_fn = '{}{:0>4d}-of-{:0>4d}.tfrecord'.format(tfrecords_prefix, shard_idx+1, num_shards)
-		tfrecord_path = os.path.join(target_dir, tfrecord_fn)
-		print("Writing {} ...".format(tfrecord_path))
-		with tf.python_io.TFRecordWriter(tfrecord_path) as writer:
-			while True:
-				im_path = path_list[i]
-				im = scipy.misc.imread(im_path)
-				# preproc
-				try:
-					im = center_crop(im, 128, 128)
-				except ValueError, e:
-					print("[Exception] {}".format(e))
-					continue
-				im = scipy.misc.imresize(im, [64, 64])
+	shard_idx = 0
+	for i, path in enumerate(path_list):
+		if i % num_per_shard == 0 and shard_idx < num_shards:
+			shard_idx += 1
+			tfrecord_fn = '{}{:0>4d}-of-{:0>4d}.tfrecord'.format(tfrecords_prefix, shard_idx, num_shards)
+			tfrecord_path = os.path.join(target_dir, tfrecord_fn)
+			print("Writing {} ...".format(tfrecord_path))
+			if shard_idx > 1:
+				writer.close()
+			writer = tf.python_io.TFRecordWriter(tfrecord_path)
 
-				example = tf.train.Example(features=tf.train.Features(feature={
-					"shape": _int64_features(im.shape),
-					"image": _bytes_features([im.tostring()])
-				}))
+		# mode='RGB' read even grayscale image as RGB shape
+		im = scipy.misc.imread(path, mode='RGB')
+		# preproc
+		try:
+			im = center_crop(im)
+		except Exception, e:
+			# print("im_path: {}".format(path))
+			# print("im_shape: {}".format(im.shape))
+			print("[Exception] {}".format(e))
+			continue
 
-				writer.write(example.SerializeToString())
+		im = scipy.misc.imresize(im, [64, 64])
+		example = tf.train.Example(features=tf.train.Features(feature={
+			"shape": _int64_features(im.shape),
+			"image": _bytes_features([im.tostring()])
+		}))
+		writer.write(example.SerializeToString())
 
-				i += 1
-				if shard_idx != num_shards-1:
-					# 마지막 샤드가 아니면 num_per_shard 에서 끝냄
-					if i % num_per_shard == 0:
-						break
-
-				# 끝까지 다 돌면 끝냄 
-				if i == num_files:
-					break
-
-		# exception 에 많이 걸리면 shard 개수를 다 못채우고 끝날수도 있음. 여기서 그걸 잡아준다.
-		if i == num_files:
-			break
+	writer.close()
 
 if __name__ == "__main__":
 	convert('./data/celebA', './data/celebA_tfrecords', exts=['jpg'], num_shards=128, tfrecords_prefix='celebA')
