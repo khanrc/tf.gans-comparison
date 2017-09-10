fd0a9db0c2be7fa8fcb9a8db6b5be1d1ee5e5ad6
khanrc <khanrc@naver.com>
2017-09-10 09:57:02 +0900

    fix tab-space mix problem

--- convert.py
+++ convert.py
@@ -28,68 +28,68 @@ def center_crop(im, output_size):
 
 
 def convert(source_dir, target_dir, crop_size, out_size, exts=[''], num_shards=128, tfrecords_prefix=''):
-	if not tf.gfile.Exists(source_dir):
-		print('source_dir does not exists')
-		return
-	
-	if tfrecords_prefix and not tfrecords_prefix.endswith('-'):
-		tfrecords_prefix += '-'
+    if not tf.gfile.Exists(source_dir):
+        print('source_dir does not exists')
+        return
+    
+    if tfrecords_prefix and not tfrecords_prefix.endswith('-'):
+        tfrecords_prefix += '-'
 
-	if tf.gfile.Exists(target_dir):
-		print("{} is Already exists".format(target_dir))
-		return
-	else:
-		tf.gfile.MakeDirs(target_dir)
+    if tf.gfile.Exists(target_dir):
+        print("{} is Already exists".format(target_dir))
+        return
+    else:
+        tf.gfile.MakeDirs(target_dir)
 
-	# get meta-data
-	path_list = []
-	for ext in exts:
-		pattern = '*.' + ext if ext != '' else '*'
-		path = os.path.join(source_dir, pattern)
-		path_list.extend(glob.glob(path))
+    # get meta-data
+    path_list = []
+    for ext in exts:
+        pattern = '*.' + ext if ext != '' else '*'
+        path = os.path.join(source_dir, pattern)
+        path_list.extend(glob.glob(path))
 
-	# shuffle path_list
-	np.random.shuffle(path_list)
-	num_files = len(path_list)
-	num_per_shard = num_files // num_shards # Last shard will have more files
+    # shuffle path_list
+    np.random.shuffle(path_list)
+    num_files = len(path_list)
+    num_per_shard = num_files // num_shards # Last shard will have more files
 
-	print('# of files: {}'.format(num_files))
-	print('# of shards: {}'.format(num_shards))
-	print('# files per shards: {}'.format(num_per_shard))
+    print('# of files: {}'.format(num_files))
+    print('# of shards: {}'.format(num_shards))
+    print('# files per shards: {}'.format(num_per_shard))
 
-	# convert to tfrecords
-	shard_idx = 0
-	for i, path in enumerate(path_list):
-		if i % num_per_shard == 0 and shard_idx < num_shards:
-			shard_idx += 1
-			tfrecord_fn = '{}{:0>4d}-of-{:0>4d}.tfrecord'.format(tfrecords_prefix, shard_idx, num_shards)
-			tfrecord_path = os.path.join(target_dir, tfrecord_fn)
-			print("Writing {} ...".format(tfrecord_path))
-			if shard_idx > 1:
-				writer.close()
-			writer = tf.python_io.TFRecordWriter(tfrecord_path)
+    # convert to tfrecords
+    shard_idx = 0
+    for i, path in enumerate(path_list):
+        if i % num_per_shard == 0 and shard_idx < num_shards:
+            shard_idx += 1
+            tfrecord_fn = '{}{:0>4d}-of-{:0>4d}.tfrecord'.format(tfrecords_prefix, shard_idx, num_shards)
+            tfrecord_path = os.path.join(target_dir, tfrecord_fn)
+            print("Writing {} ...".format(tfrecord_path))
+            if shard_idx > 1:
+                writer.close()
+            writer = tf.python_io.TFRecordWriter(tfrecord_path)
 
-		# mode='RGB' read even grayscale image as RGB shape
-		im = scipy.misc.imread(path, mode='RGB')
-		# preproc
-		try:
-			im = center_crop(im, crop_size)
-		except Exception, e:
-			# print("im_path: {}".format(path))
-			# print("im_shape: {}".format(im.shape))
-			print("[Exception] {}".format(e))
-			continue
+        # mode='RGB' read even grayscale image as RGB shape
+        im = scipy.misc.imread(path, mode='RGB')
+        # preproc
+        try:
+            im = center_crop(im, crop_size)
+        except Exception, e:
+            # print("im_path: {}".format(path))
+            # print("im_shape: {}".format(im.shape))
+            print("[Exception] {}".format(e))
+            continue
 
-		im = scipy.misc.imresize(im, out_size)
-		example = tf.train.Example(features=tf.train.Features(feature={
-			"shape": _int64_features(im.shape),
-			"image": _bytes_features([im.tostring()])
-		}))
-		writer.write(example.SerializeToString())
+        im = scipy.misc.imresize(im, out_size)
+        example = tf.train.Example(features=tf.train.Features(feature={
+            "shape": _int64_features(im.shape),
+            "image": _bytes_features([im.tostring()])
+        }))
+        writer.write(example.SerializeToString())
 
-	writer.close()
+    writer.close()
 
 
 if __name__ == "__main__":
-	convert('./data/celebA', './data/celebA_tfrecords_test', crop_size=[128, 128], out_size=[64, 64], exts=['jpg'], 
+    convert('./data/celebA', './data/celebA_tfrecords_test', crop_size=[128, 128], out_size=[64, 64], exts=['jpg'], 
         num_shards=128, tfrecords_prefix='celebA')
